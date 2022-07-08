e08a2acef4a7a69cf23a2148c0e666a92827ec98
Chen Shalev <chen.shalev@exlibrisgroup.com>
2022-07-08 21:39:51 +0300

    Re-order files

--- index.js
+++ index.js
@@ -1,49 +1,72 @@
-import fetchFrom from './src/http.js';
-import {getSitemapsFromIndexURL} from './src/xml.js';
+import fetchFrom from "./src/http.js";
+import { getSitemapsFromIndexURL } from "./src/xml.js";
 
-import xml2json from 'xml2js';
+import xml2json from "xml2js";
 // Readline
-import * as readline from 'node:readline/promises';
+import * as readline from "node:readline/promises";
+const { exec } = require("child_process");
+exec("node.bat", (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const siteMapURL = await rl.question("Input sitemap URL: ");
+  rl.close();
+  const rl1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const identifier = await rl1.question("Insert MMSID or UR identifier: ");
+  rl1.close();
+  console.log("Please wait for the search resutls....");
+  async function mapSitemaps(sitemap) {
+    try {
+      const siteMapXML = await fetchFrom(sitemap);
+      const parsedXMLSitemap = await xml2json.parseStringPromise(siteMapXML);
+      return parsedXMLSitemap.urlset.url
+        .flatMap((entry) => entry.loc)
+        .flatMap((loc) => {
+          const assetId = loc.split("/").pop();
+          return {
+            loc,
+            assetId,
+          };
+        })
+        .reduce((acc, currentItem) => {
+          const parameter =
+            typeof identifier === "string"
+              ? String(identifier)
+              : Number(MMSID_or_Parameter);
+          if (currentItem.assetId === parameter) {
+            acc = [...acc, { location: currentItem.loc, sitemap }];
+          }
+          return acc;
+        }, []);
+    } catch (error) {
+      console.log({
+        error,
+        origin: "[index] fetchFrom(sitemap)",
+        parameters: [{ sitemap, siteMapXML, parsedXMLSitemap }],
+      });
+    }
+  }
+  async function searchIdentifier() {
+    try {
+      const sitemapsURL = await fetchFrom(siteMapURL);
+      const sitemaps = await getSitemapsFromIndexURL(sitemapsURL);
+      const mappedSitemaps = await Promise.all(sitemaps.map(mapSitemaps));
+      const [result] = mappedSitemaps.flat(Infinity);
+      console.log(result);
+    } catch (error) {
+      //console.log({error, origin: '[index] searchIdentifier()', parameters: undefined});
+    }
+  }
+  
+  searchIdentifier();
+  console.log(stdout);
+});
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout});
-const siteMapURL = await rl.question('Input sitemap URL: ');
-rl.close();
-const rl1 = readline.createInterface({ input: process.stdin, output: process.stdout});
-const identifier = await rl1.question('Insert MMSID or UR identifier: ')
-rl1.close();
-console.log('Please wait for the search resutls....')
-async function mapSitemaps(sitemap) {
-	try {
-		const siteMapXML = await fetchFrom(sitemap);
-		const parsedXMLSitemap = await xml2json.parseStringPromise(siteMapXML);
-		return parsedXMLSitemap.urlset.url
-			.flatMap(entry => entry.loc)
-			.flatMap(loc => {
-        const assetId = loc.split('/').pop();
-        return {
-				loc,
-				assetId
-			}})
-			.reduce((acc, currentItem) => {
-	        const parameter = typeof identifier === 'string' ?  String(identifier) : Number(MMSID_or_Parameter); 
-			if (currentItem.assetId === parameter) {
-          	acc = [...acc, { location: currentItem.loc, sitemap}];
-			}
-				return acc;
-			}, []);
-			} catch (error) {
-			console.log({error, origin: '[index] fetchFrom(sitemap)', parameters: [{ sitemap, siteMapXML, parsedXMLSitemap }]});
-	}
-}
-async function searchIdentifier() {
-	try {
- 		const sitemapsURL = await fetchFrom(siteMapURL);   
-		const sitemaps = await getSitemapsFromIndexURL(sitemapsURL);
- 		const mappedSitemaps = await Promise.all(sitemaps.map(mapSitemaps));
-		const [result] = mappedSitemaps.flat(Infinity);
-		console.log(result);
-	} catch (error) {
-    //console.log({error, origin: '[index] searchIdentifier()', parameters: undefined});
-	}
-}
-searchIdentifier();
